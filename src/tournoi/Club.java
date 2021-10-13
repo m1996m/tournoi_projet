80035679b2b4a408c9236188b6c46c0f352e9a4c
m1996m <mmd1996.m@gmail.com>
2021-10-13 09:44:54 +0200

    resolution du deuxieme test

--- src/tournoi/Club.java
+++ src/tournoi/Club.java
@@ -50,4 +50,41 @@ public class Club {
 		this.setSession(new Session());
 		
 	}
+	//fonciton qui permet de creer une liste 20 joueur
+	public List<Joueur> tabJoueur(int nombreJoueur){
+		List<Joueur> joueur =new ArrayList<Joueur>();
+		for(int i=0;i<nombreJoueur;i++) {
+			int p=40;
+			Joueur J= new Joueur(i,"nom"+i,"prenom"+i,"adresse"+i,p,2021);
+			joueur.add(J);
+			p+=3;
+			
+		}
+		return joueur;	
+	}
+	
+	//fonciton qui permet de partager les 20 joueurs entre les deux equipe
+	public void remplissageJoueurJoueur(Equipe equipe1, Equipe equipe2,Club club,int nombreJoueur) {
+		List<Joueur> joueur =new ArrayList<Joueur>();
+		joueur=tabJoueur(nombreJoueur);
+		equipe1.setNombreJoueur(0);
+		equipe1.setNombrePoids(0);
+		equipe2.setNombreJoueur(0);
+		equipe2.setNombrePoids(0);
+		for(int i=0;i<nombreJoueur;i++){
+			if(i<joueur.size()/2) {
+				equipe1.getJoueur().add(joueur.get(0));
+				equipe1.setNombreJoueur(equipe1.getNombreJoueur()+1);
+				equipe1.setNombrePoids(equipe1.getNombrePoids()+1);
+			}else {
+				equipe2.getJoueur().add(joueur.get(1));
+				equipe2.setNombreJoueur(equipe2.getNombreJoueur()+1);
+				equipe2.setNombrePoids(equipe2.getNombrePoids()+1);
+				
+			}
+			
+		}
+		
+		
+	}
 }
