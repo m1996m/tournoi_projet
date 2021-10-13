6952fc2982db9180724fce1bdfff63c59f4afa78
m1996m <mmd1996.m@gmail.com>
2021-10-13 08:47:36 +0200

    premierTest

--- src/tournoi/Club.java
+++ src/tournoi/Club.java
@@ -3,7 +3,6 @@ package tournoi;
 import java.util.ArrayList;
 import java.util.List;
 
-
 /**
  * @author DELL
  * Une classe permetttant de creer un club
@@ -35,4 +34,20 @@ public class Club {
 	public void setSession(Session session) {
 		this.session = session;
 	}
+	
+	//fonction permettant d'ajouter une equipe qui re�oit en parametre une session
+	/*public void creerEquipe() {
+		
+		Equipe equipe1=new Equipe();
+		Equipe equipe2=new Equipe();
+		List<Equipe> l=new ArrayList<Equipe>();
+		l.add(equipe1);
+		l.add(equipe2);
+		this.setEquipe(l);	
+	}
+	//fonciton qu i permet de creer une session qui re �oit en parametre une session 
+	public void CreerSession() {
+		this.setSession(new Session());
+		
+	}*/
 }
