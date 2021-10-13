4bb2ba239ef7a0493364e0846cf39853cf08fbef
m1996m <mmd1996.m@gmail.com>
2021-10-13 09:58:18 +0200

    Ce troisieme testpermet de verifier si une session d'un club contient des manches et contient les deux equipes

--- src/tournoi/JunitTest.java
+++ src/tournoi/JunitTest.java
@@ -35,5 +35,21 @@ class JunitTest {
 		assertNotNull("Aucun joueur n'a �t� pas enregistr�", club.getEquipe().get(0).getJoueur().get(0));
 		assertNotNull("Aucun joueur n'a �t� pas enregistr�", club.getEquipe().get(1).getJoueur().get(0));
 	}
+	
+	//Un test qui permet de verifier si manche existe dans Session d'un club.
+	@Test
+	public void testMancheExisteDansSession() {
+		club.CreerSession();
+		assertNotNull("Aucune manche n'a ete pas cree", club.getSession().getNombreManche());
+	}
+	
+	//Un test qui permet de verifier si les deux equipes existent dans Session d'un club.
+	@Test
+	public void testEquipe1Equipe2ExisteDansSession() {
+		club.CreerSession();
+		club.creerEquipe();
+		assertNotNull("Aucune manche n'a ete pas cree", club.getSession().getEquipe1());
+		assertNotNull("Aucune manche n'a ete pas cree", club.getSession().getEquipe2());
+	}
 
 }
